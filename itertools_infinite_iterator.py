d3f18e5d9bff57f0b8370b58e21d42ac8d29edcc
Sanchit <sanchit10gawde@gmail.com>
2026-02-09 23:18:06 +0530

    itertools

--- itertools_infinite_iterator.py
+++ itertools_infinite_iterator.py
@@ -27,8 +27,31 @@ for i in cycle(a):
         break
     print(i)
 
+#OP:
+# hello
+# word
+# how r you
+# hello
+# word
+# how r you
+# hello
+# word
+# how r you
+
 # repeat
 # infinite time give value is repeated
 for i in repeat("hello",10):
     # in this case hello will be repeated 10 times, if 10 was not givcn it will do infinite
     print(i)
+
+#OP:
+# hello
+# hello
+# hello
+# hello
+# hello
+# hello
+# hello
+# hello
+# hello
+# hello
